156498141c6829e49a086e6dd04f775ebb98a3ed
Moe Steinmueller <0101.mste@gmail.com>
2021-03-25 16:47:03 +0100

    2nd exercise done

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ console.log('Starting javascript...');
 
 // Exercise 1: Print my name
 var myName = 'Moe Steinmueller';
-console.log(myName);
+console.log('my name is: ', myName);
+
+// Exercise 2:
+var age = 28;
+console.log('my age is: ', age);
