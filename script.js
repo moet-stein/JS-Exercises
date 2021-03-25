af92daf6c205291c5feea4473c0487d6cad42330
Moe Steinmueller <0101.mste@gmail.com>
2021-03-25 16:50:45 +0100

    3rd exercise done

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ console.log('Starting javascript...');
 var myName = 'Moe Steinmueller';
 console.log('my name is: ', myName);
 
-// Exercise 2:
+// Exercise 2: Print my age
 var age = 28;
 console.log('my age is: ', age);
+
+// Exercise 3: Calc. and Print the age difference
+var juliaAge = 32;
+var ageDiff = age - juliaAge;
+console.log('Age difference is: ', ageDiff);
